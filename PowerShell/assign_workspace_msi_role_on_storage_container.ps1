fe960c9612b87857d90a2fc5313b528620027ccb
Saveen Reddy <saveenr@live.com>
2020-04-17 12:11:01 -0700

    Create assign_workspace_msi_role_on_storage_container.ps1

---
+++ PowerShell/assign_workspace_msi_role_on_storage_container.ps1
@@ -0,0 +1,9 @@
+$wsmsi_objid = "xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx" # Replace with Workspace MSI object id here
+$stg_subid = "xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx" # Replace with ID of Subscription that the Storage Account is in
+$stg_account = "name_of_storage_account" # Replace with Storage account name
+$stg_container = "name_of_container" # Replace with the container to give the MSI access to
+$role = "Storage Blob Data Contributor" # Leave this alone
+
+$scope = "/subscriptions/" + $stg_subid  + "/resourceGroups/shared/providers/Microsoft.Storage/storageAccounts/"
+$scope = $scope + $stg_account + "/blobServices/default/containers/" + "users"
+New-AzRoleAssignment -ObjectId $wsmsi_objid -RoleDefinitionName $role -Scope $scope 
