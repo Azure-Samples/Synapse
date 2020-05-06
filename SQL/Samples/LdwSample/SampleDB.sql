8b32a0611abdcc5bc0dc2b3b0ec6d0157fda36f3
Jovan Popovic <jocapc@gmail.com>
2020-05-06 23:20:38 +0200

    Added database scoped credentials

--- SQL/Samples/LdwSample/SampleDB.sql
+++ SQL/Samples/LdwSample/SampleDB.sql
@@ -1,4 +1,7 @@
-
+------------------------------------------------------------------------------------------
+--      Part 1 - Cleanup script
+--      This part removes objects from sample database
+------------------------------------------------------------------------------------------
 DROP VIEW IF EXISTS parquet.NYCTaxi
 GO
 DROP VIEW IF EXISTS json.Books
@@ -8,8 +11,16 @@ GO
 IF (EXISTS(SELECT * FROM sys.external_tables WHERE name = 'Population')) BEGIN
     DROP EXTERNAL TABLE csv.Population
 END
-IF (EXISTS(SELECT * FROM sys.external_file_formats WHERE name = 'QuotedCsvWithHeader')) BEGIN
-    DROP EXTERNAL FILE FORMAT QuotedCsvWithHeader
+IF (EXISTS(SELECT * FROM sys.external_file_formats WHERE name = 'QuotedCsvWithHeaderFormat')) BEGIN
+    DROP EXTERNAL FILE FORMAT QuotedCsvWithHeaderFormat
+END
+GO
+IF (EXISTS(SELECT * FROM sys.external_file_formats WHERE name = 'QuotedCsvWithoutHeaderFormat')) BEGIN
+    DROP EXTERNAL FILE FORMAT QuotedCsvWithoutHeaderFormat
+END
+GO
+IF (EXISTS(SELECT * FROM sys.external_file_formats WHERE name = 'ParquetFormat')) BEGIN
+    DROP EXTERNAL FILE FORMAT ParquetFormat
 END
 GO
 DROP SCHEMA IF EXISTS parquet;
@@ -23,11 +34,13 @@ IF (EXISTS(SELECT * FROM sys.external_data_sources WHERE name = 'SqlOnDemandDemo
     DROP EXTERNAL DATA SOURCE SqlOnDemandDemo
 END
 
-
-IF (EXISTS(SELECT * FROM sys.external_data_sources WHERE name = 'SqlOnDemandDemo')) BEGIN
-    DROP EXTERNAL DATA SOURCE SqlOnDemandDemo
+IF (EXISTS(SELECT * FROM sys.external_data_sources WHERE name = 'AzureOpenDataStorage')) BEGIN
+    DROP EXTERNAL DATA SOURCE AzureOpenDataStorage
 END
 
+IF (EXISTS(SELECT * FROM sys.external_data_sources WHERE name = 'YellowTaxi')) BEGIN
+    DROP EXTERNAL DATA SOURCE YellowTaxi
+END
 
 IF EXISTS
    (SELECT * FROM sys.credentials
@@ -35,12 +48,50 @@ IF EXISTS
    DROP CREDENTIAL [https://sqlondemandstorage.blob.core.windows.net]
 GO
 
+IF EXISTS
+   (SELECT * FROM sys.database_scoped_credentials
+   WHERE name = 'sqlondemand')
+   DROP DATABASE SCOPED CREDENTIAL [sqlondemand]
+GO
 
--- create credentials for the containers in demo storage account
+IF EXISTS
+   (SELECT * FROM sys.database_scoped_credentials
+   WHERE name = 'AadIdentity')
+   DROP DATABASE SCOPED CREDENTIAL [AadIdentity]
+GO
+
+IF EXISTS
+   (SELECT * FROM sys.database_scoped_credentials
+   WHERE name = 'WorkspaceIdentity')
+   DROP DATABASE SCOPED CREDENTIAL [WorkspaceIdentity]
+GO
+
+
+------------------------------------------------------------------------------------------
+--      Part 2 - initialization script
+--      This part creates required objects in sample database
+------------------------------------------------------------------------------------------
+
+-- create server-scoped credential for the containers in demo storage account
+-- this credential will be used in OPENROWSET function without data source that uses absolute file URL
 CREATE CREDENTIAL [https://sqlondemandstorage.blob.core.windows.net]
 WITH IDENTITY='SHARED ACCESS SIGNATURE',  
 SECRET = 'sv=2018-03-28&ss=bf&srt=sco&sp=rl&st=2019-10-14T12%3A10%3A25Z&se=2061-12-31T12%3A10%3A00Z&sig=KlSU2ullCscyTS0An0nozEpo4tO5JAgGBvw%2FJX2lguw%3D'
 GO
+
+-- create database-scoped credential for the containers in demo storage account
+-- this credential will be used in OPENROWSET function with data source that uses relative file URL
+CREATE DATABASE SCOPED CREDENTIAL [sqlondemand]
+WITH IDENTITY='SHARED ACCESS SIGNATURE',  
+SECRET = 'sv=2018-03-28&ss=bf&srt=sco&sp=rl&st=2019-10-14T12%3A10%3A25Z&se=2061-12-31T12%3A10%3A00Z&sig=KlSU2ullCscyTS0An0nozEpo4tO5JAgGBvw%2FJX2lguw%3D'
+GO
+-- Create credential that will allow AAD user to impersonate
+CREATE DATABASE SCOPED CREDENTIAL AadIdentity WITH IDENTITY = 'User Identity'
+GO
+-- Create credential that will allow user to impersonate using Managed Identity assigned to workspace
+CREATE DATABASE SCOPED CREDENTIAL WorkspaceIdentity WITH IDENTITY = 'Managed Identity'
+GO
+
 
 CREATE SCHEMA parquet;
 GO
@@ -49,13 +100,21 @@ GO
 CREATE SCHEMA json;
 GO
 
--- create sample external table
+-- Create external data source secured using credential
 CREATE EXTERNAL DATA SOURCE SqlOnDemandDemo WITH (
-    LOCATION = 'https://sqlondemandstorage.blob.core.windows.net'
+    LOCATION = 'https://sqlondemandstorage.blob.core.windows.net',
+    CREDENTIAL = sqlondemand
 );
 GO
+-- Create publicly available external data sources
+CREATE EXTERNAL DATA SOURCE AzureOpenDataStorage
+WITH ( LOCATION = 'https://azureopendatastorage.blob.core.windows.net/')
+GO
+CREATE EXTERNAL DATA SOURCE YellowTaxi
+WITH ( LOCATION = 'https://azureopendatastorage.blob.core.windows.net/nyctlc/yellow/')
 
-CREATE EXTERNAL FILE FORMAT QuotedCsvWithHeader
+
+CREATE EXTERNAL FILE FORMAT QuotedCsvWithHeaderFormat
 WITH (  
     FORMAT_TYPE = DELIMITEDTEXT,
     FORMAT_OPTIONS (
@@ -63,6 +122,21 @@ WITH (
         STRING_DELIMITER = '"',
         FIRST_ROW = 2
     )
+);
+GO
+CREATE EXTERNAL FILE FORMAT QuotedCsvWithoutHeaderFormat
+WITH (  
+    FORMAT_TYPE = DELIMITEDTEXT,
+    FORMAT_OPTIONS (
+        FIELD_TERMINATOR = ',',
+        STRING_DELIMITER = '"',
+        FIRST_ROW = 1
+    )
+);
+GO
+CREATE EXTERNAL FILE FORMAT ParquetFormat
+WITH (  
+    FORMAT_TYPE = PARQUET
 );
 GO
 
@@ -76,7 +150,7 @@ CREATE EXTERNAL TABLE csv.population
 WITH (
     LOCATION = 'csv/population/population.csv',
     DATA_SOURCE = SqlOnDemandDemo,
-    FILE_FORMAT = QuotedCsvWithHeader
+    FILE_FORMAT = QuotedCsvWithHeaderFormat
 );
 GO
 
