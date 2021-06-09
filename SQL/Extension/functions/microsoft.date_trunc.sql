6ee6e07e93e8bc12a064f73acdd381a99a6952b8
Matt Usher <mausher@microsoft.com>
2021-06-09 01:29:16 -0700

    Updating the DATE_TRUNC and FIRSTDAYOFQUARTER functions

--- SQL/Extension/functions/microsoft.date_trunc.sql
+++ SQL/Extension/functions/microsoft.date_trunc.sql
@@ -2,13 +2,7 @@ IF EXISTS (SELECT * FROM sys.objects WHERE schema_id=SCHEMA_ID('microsoft') AND 
     DROP FUNCTION microsoft.date_trunc;
 GO
 
-/*
-Note: The DATEDIFF function returns a 4-byte integer (int). Until Azure Synapse SQL supports DATEDIFF_BIG 
-	  with an 8-byte integer return type, date_trunc can and likely will throw an overflow exception when
-	  truncating small date_part units like second or smaller.
-*/
-
-CREATE FUNCTION microsoft.date_trunc(@unit VARCHAR(15), @expression VARCHAR(8000))
+CREATE FUNCTION microsoft.date_trunc(@unit VARCHAR(15), @expression DATETIME2)
 RETURNS DATETIME2
 WITH SCHEMABINDING
 AS
@@ -16,49 +10,53 @@ BEGIN
 
 	-- Declarations
 	DECLARE @result DATETIME2;
+    DECLARE @template VARCHAR(27)
 
 	SET @unit = UPPER(@unit);
 
+	-- Special handle Quarter and Week
+	IF (@unit = 'WEEK')
+		RETURN DATEADD(DAY, -(DATEPART(WEEKDAY, @expression) - 1), DATEADD(DAY, DATEDIFF(DAY, 0, @expression), 0) )
+
+	IF (@unit = 'QUARTER')
+		RETURN microsoft.FirstDayOfQuarter(@expression);
+
 	SET
-		@result = 
+		@template = 
 		(
 			CASE
 				-- Year
-				WHEN @unit = 'Y' OR @unit = 'YEAR' THEN DATEADD(YEAR, DATEDIFF(YEAR, 0, @expression), 0) 
+				WHEN @unit = 'Y' OR @unit = 'YEAR' THEN '01/01/yyyy'
 
 				-- Month
-				WHEN @unit = 'M' OR @unit = 'MONTH' THEN DATEADD(MONTH, DATEDIFF(MONTH, 0, @expression), 0) 
+				WHEN @unit = 'M' OR @unit = 'MONTH' THEN 'MM/01/yyyy'
 
 				-- Day
-				WHEN @unit = 'D' OR @unit = 'DAY' THEN DATEADD(DAY, DATEDIFF(DAY, 0, @expression), 0) 
+				WHEN @unit = 'D' OR @unit = 'DAY' THEN 'MM/dd/yyyy'
 
 				-- Hour
-				WHEN @unit = 'H' OR @unit = 'HOUR' THEN DATEADD(HOUR, DATEDIFF(HOUR, 0, @expression), 0) 
+				WHEN @unit = 'H' OR @unit = 'HOUR' THEN 'MM/dd/yyyy hh:00:00.000000'
 
 				-- Minute
-				WHEN @unit = 'MINUTE' THEN DATEADD(MINUTE, DATEDIFF(MINUTE, 0, @expression), 0) 
+				WHEN @unit = 'MINUTE' THEN 'MM/dd/yyyy hh:mm:00.000000'
 
 				-- Second
-				WHEN @unit = 'S' OR @unit = 'SECOND' THEN DATEADD(SECOND, DATEDIFF(SECOND, 0, @expression), 0) 
+				WHEN @unit = 'S' OR @unit = 'SECOND' THEN 'MM/dd/yyyy hh:mm:ss.000000'
 
 				-- Millisecond 
-				WHEN @unit = 'MS' OR @unit = 'MILLISECOND' THEN DATEADD(MILLISECOND, DATEDIFF(MILLISECOND, 0, @expression), 0) 
+				WHEN @unit = 'MS' OR @unit = 'MILLISECOND' THEN 'MM/dd/yyyy hh:mm:ss.ff0000'
 
 				-- Microsecond 
-				WHEN @unit = 'MICROSECOND' THEN DATEADD(MICROSECOND, DATEDIFF(MICROSECOND, 0, @expression), 0) 
+				WHEN @unit = 'MICROSECOND' THEN 'MM/dd/yyyy hh:mm:ss.ffffff'
 
 				-- Nanosecond 
-				WHEN @unit = 'NANOSECOND' THEN DATEADD(NANOSECOND, DATEDIFF(NANOSECOND, 0, @expression), 0) 
+				-- Note: Nanosecond is not a supported datetime unit in the DATEADD function
 
-				-- Week
-				WHEN @unit = 'W' OR @unit = 'WEEK' THEN DATEADD(DAY, -(DATEPART(WEEKDAY, @expression) - 1), DATEADD(DAY, DATEDIFF(DAY, 0, @expression), 0) )
-
-				-- Quarter
-				WHEN @unit = 'QUARTER' THEN microsoft.FirstDayOfQuarter(@expression)
+				ELSE 'MM/dd/yyyy hh:mm:ss.fffffff'
 			END
 		);
 
-	RETURN @result;
+	RETURN FORMAT(@expression, @template);
 
 END
 GO
